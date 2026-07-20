4e1ca413d006d0e5900f12f1d5cba9c8bdf5b2d4
Darshan <darshan@Darshans-MacBook-Air.local>
2026-07-20 09:44:46 +0530

    Python Lab Practical 2.1

---
+++ Pthyhon 3rd Sem/Practical2(1).py
@@ -0,0 +1,8 @@
+num = float(input("Enter the number : "))
+
+if num > 0 : 
+    print ("The entered number ",num,"is positive")
+elif num < 0 : 
+    print ("The entered number ",num,"is negative.")
+else : 
+    print ("The entered number ",num,"is Zero. ")
