4a71561721295d7112396ca847353c6d70d832cf
Darshan <darshan@Darshans-MacBook-Air.local>
2026-08-02 10:31:53 +0530

    Python Practical 3.3

--- Pthyhon 3rd Sem/Practical3(3).py
+++ Pthyhon 3rd Sem/Practical3(3).py
@@ -1,13 +1,18 @@
 # Practical 3.3 : Write a Python program to display all prime numbers between two given numbers.
 
-num = int(input("Enter the number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 i = 2
+while num1 <= num2:
+    i = 2
 
-while i <= num/2:
-    if num % i == 0:
-        print(num, "is not a prime number")
-        break
-    i += 1
-else : 
-    print(num, "is a prime number")
+    while i <= num1 / 2:
+        if num1 % i == 0:
+            break
+        i += 1
+
+    if i > num1 / 2 and num1 > 1:
+        print(num1)
+
+    num1 += 1
